d24e47c9a8fa9753a5e6cae69984b7a7333e281b
jontysh4w <jontyshaw@btinternet.com>
2024-08-28 09:22:59 +0100

    changed refresh button to post on frontend to match backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -29,7 +29,7 @@ export const refreshStockData = async (symbol) => {
     // const response = await axios.get(`${API_BASE_URL}/${symbol}/refresh`); <- refresh changes data to getandUpdateStocks, 
     // can't debug getting new responses unless I need to give Alpha Vantage my whitelist IP or another API key issues
     // MongoDB is not giving repsonses as it should 
-    const response = await axios.get(`${API_BASE_URL}/${symbol}`);
+    const response = await axios.post(`${API_BASE_URL}/${symbol}/refresh`);
     return response.data;
   } catch (error) {
     console.error('Error in refreshStockData:', error.response?.data || error.message);
